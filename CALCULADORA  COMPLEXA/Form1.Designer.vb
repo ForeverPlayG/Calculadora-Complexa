06a892cd7c8997e87c51f3d991961e7851b0b3b8
ForeverPlayG <hask.oder@gmail.com>
2024-11-03 18:15:18 +0000

    Calculadora03/11

---
+++ CALCULADORA  COMPLEXA/Form1.Designer.vb
@@ -0,0 +1,488 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        TxtBox = New TextBox()
+        Btt1 = New Button()
+        Btt2 = New Button()
+        Btt5 = New Button()
+        Btt4 = New Button()
+        Btt3 = New Button()
+        Btt8 = New Button()
+        Btt6 = New Button()
+        Btt0 = New Button()
+        Btt9 = New Button()
+        Btt7 = New Button()
+        BttVEZES = New Button()
+        BttIGUAL = New Button()
+        BttMAIS = New Button()
+        BttSQR = New Button()
+        BttMENOS = New Button()
+        BttDIVIDIR = New Button()
+        BttC = New Button()
+        BttPONTO = New Button()
+        BttELEVADO = New Button()
+        BttSIN = New Button()
+        BttCOS = New Button()
+        BttPI = New Button()
+        BttFR = New Button()
+        BttSQRELE = New Button()
+        BttCOS1 = New Button()
+        BttTAN = New Button()
+        BttTAN1 = New Button()
+        BttSIN1 = New Button()
+        BttRAD = New Button()
+        BttGRAUS = New Button()
+        BttCONTAGENS = New Button()
+        BttLOG = New Button()
+        BttSD = New Button()
+        BttPERMUTACOES = New Button()
+        BttPERCENTAGEM = New Button()
+        BttFATORIAL = New Button()
+        SuspendLayout()
+        ' 
+        ' TxtBox
+        ' 
+        TxtBox.Location = New Point(134, 81)
+        TxtBox.Multiline = True
+        TxtBox.Name = "TxtBox"
+        TxtBox.Size = New Size(230, 45)
+        TxtBox.TabIndex = 0
+        TxtBox.Text = "0"
+        TxtBox.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Btt1
+        ' 
+        Btt1.BackColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Btt1.BackgroundImage = CType(resources.GetObject("Btt1.BackgroundImage"), Image)
+        Btt1.Location = New Point(171, 141)
+        Btt1.Name = "Btt1"
+        Btt1.Size = New Size(45, 49)
+        Btt1.TabIndex = 1
+        Btt1.Text = "1"
+        Btt1.UseVisualStyleBackColor = False
+        ' 
+        ' Btt2
+        ' 
+        Btt2.Location = New Point(224, 141)
+        Btt2.Name = "Btt2"
+        Btt2.Size = New Size(45, 49)
+        Btt2.TabIndex = 2
+        Btt2.Text = "2"
+        Btt2.UseVisualStyleBackColor = True
+        ' 
+        ' Btt5
+        ' 
+        Btt5.Location = New Point(224, 205)
+        Btt5.Name = "Btt5"
+        Btt5.Size = New Size(45, 49)
+        Btt5.TabIndex = 3
+        Btt5.Text = "5"
+        Btt5.UseVisualStyleBackColor = True
+        ' 
+        ' Btt4
+        ' 
+        Btt4.Location = New Point(171, 205)
+        Btt4.Name = "Btt4"
+        Btt4.Size = New Size(45, 49)
+        Btt4.TabIndex = 4
+        Btt4.Text = "4"
+        Btt4.UseVisualStyleBackColor = True
+        ' 
+        ' Btt3
+        ' 
+        Btt3.Location = New Point(275, 141)
+        Btt3.Name = "Btt3"
+        Btt3.Size = New Size(45, 49)
+        Btt3.TabIndex = 5
+        Btt3.Text = "3"
+        Btt3.UseVisualStyleBackColor = True
+        ' 
+        ' Btt8
+        ' 
+        Btt8.Location = New Point(224, 269)
+        Btt8.Name = "Btt8"
+        Btt8.Size = New Size(45, 49)
+        Btt8.TabIndex = 6
+        Btt8.Text = "8"
+        Btt8.UseVisualStyleBackColor = True
+        ' 
+        ' Btt6
+        ' 
+        Btt6.Location = New Point(275, 205)
+        Btt6.Name = "Btt6"
+        Btt6.Size = New Size(45, 49)
+        Btt6.TabIndex = 7
+        Btt6.Text = "6"
+        Btt6.UseVisualStyleBackColor = True
+        ' 
+        ' Btt0
+        ' 
+        Btt0.Location = New Point(224, 331)
+        Btt0.Name = "Btt0"
+        Btt0.Size = New Size(45, 42)
+        Btt0.TabIndex = 0
+        Btt0.Text = "0"
+        Btt0.UseVisualStyleBackColor = True
+        ' 
+        ' Btt9
+        ' 
+        Btt9.Location = New Point(275, 269)
+        Btt9.Name = "Btt9"
+        Btt9.Size = New Size(45, 49)
+        Btt9.TabIndex = 9
+        Btt9.Text = "9"
+        Btt9.UseVisualStyleBackColor = True
+        ' 
+        ' Btt7
+        ' 
+        Btt7.Location = New Point(171, 269)
+        Btt7.Name = "Btt7"
+        Btt7.Size = New Size(45, 49)
+        Btt7.TabIndex = 10
+        Btt7.Text = "7"
+        Btt7.UseVisualStyleBackColor = True
+        ' 
+        ' BttVEZES
+        ' 
+        BttVEZES.Location = New Point(470, 81)
+        BttVEZES.Name = "BttVEZES"
+        BttVEZES.Size = New Size(61, 42)
+        BttVEZES.TabIndex = 11
+        BttVEZES.Text = "X"
+        BttVEZES.UseVisualStyleBackColor = True
+        ' 
+        ' BttIGUAL
+        ' 
+        BttIGUAL.Location = New Point(403, 81)
+        BttIGUAL.Name = "BttIGUAL"
+        BttIGUAL.Size = New Size(61, 42)
+        BttIGUAL.TabIndex = 12
+        BttIGUAL.Text = "="
+        BttIGUAL.UseVisualStyleBackColor = True
+        ' 
+        ' BttMAIS
+        ' 
+        BttMAIS.Location = New Point(403, 132)
+        BttMAIS.Name = "BttMAIS"
+        BttMAIS.Size = New Size(61, 42)
+        BttMAIS.TabIndex = 13
+        BttMAIS.Text = "+"
+        BttMAIS.UseVisualStyleBackColor = True
+        ' 
+        ' BttSQR
+        ' 
+        BttSQR.Location = New Point(470, 184)
+        BttSQR.Name = "BttSQR"
+        BttSQR.Size = New Size(61, 42)
+        BttSQR.TabIndex = 14
+        BttSQR.Text = "√"
+        BttSQR.UseVisualStyleBackColor = True
+        ' 
+        ' BttMENOS
+        ' 
+        BttMENOS.Location = New Point(403, 184)
+        BttMENOS.Name = "BttMENOS"
+        BttMENOS.Size = New Size(61, 42)
+        BttMENOS.TabIndex = 15
+        BttMENOS.Text = "-"
+        BttMENOS.UseVisualStyleBackColor = True
+        ' 
+        ' BttDIVIDIR
+        ' 
+        BttDIVIDIR.Location = New Point(470, 132)
+        BttDIVIDIR.Name = "BttDIVIDIR"
+        BttDIVIDIR.Size = New Size(61, 42)
+        BttDIVIDIR.TabIndex = 16
+        BttDIVIDIR.Text = ":"
+        BttDIVIDIR.UseVisualStyleBackColor = True
+        ' 
+        ' BttC
+        ' 
+        BttC.Location = New Point(171, 331)
+        BttC.Name = "BttC"
+        BttC.Size = New Size(45, 42)
+        BttC.TabIndex = 17
+        BttC.Text = "c"
+        BttC.UseVisualStyleBackColor = True
+        ' 
+        ' BttPONTO
+        ' 
+        BttPONTO.Location = New Point(275, 331)
+        BttPONTO.Name = "BttPONTO"
+        BttPONTO.Size = New Size(45, 42)
+        BttPONTO.TabIndex = 18
+        BttPONTO.Text = ","
+        BttPONTO.UseVisualStyleBackColor = True
+        ' 
+        ' BttELEVADO
+        ' 
+        BttELEVADO.Location = New Point(470, 288)
+        BttELEVADO.Name = "BttELEVADO"
+        BttELEVADO.Size = New Size(61, 42)
+        BttELEVADO.TabIndex = 24
+        BttELEVADO.Text = "e^"
+        BttELEVADO.UseVisualStyleBackColor = True
+        ' 
+        ' BttSIN
+        ' 
+        BttSIN.Location = New Point(403, 340)
+        BttSIN.Name = "BttSIN"
+        BttSIN.Size = New Size(61, 42)
+        BttSIN.TabIndex = 23
+        BttSIN.Text = "sin"
+        BttSIN.UseVisualStyleBackColor = True
+        ' 
+        ' BttCOS
+        ' 
+        BttCOS.Location = New Point(470, 340)
+        BttCOS.Name = "BttCOS"
+        BttCOS.Size = New Size(61, 42)
+        BttCOS.TabIndex = 22
+        BttCOS.Text = "cos"
+        BttCOS.UseVisualStyleBackColor = True
+        ' 
+        ' BttPI
+        ' 
+        BttPI.Location = New Point(403, 288)
+        BttPI.Name = "BttPI"
+        BttPI.Size = New Size(61, 42)
+        BttPI.TabIndex = 21
+        BttPI.Text = "π"
+        BttPI.UseVisualStyleBackColor = True
+        ' 
+        ' BttFR
+        ' 
+        BttFR.Location = New Point(403, 237)
+        BttFR.Name = "BttFR"
+        BttFR.Size = New Size(61, 42)
+        BttFR.TabIndex = 20
+        BttFR.Text = "FR"
+        BttFR.UseVisualStyleBackColor = True
+        ' 
+        ' BttSQRELE
+        ' 
+        BttSQRELE.Location = New Point(470, 237)
+        BttSQRELE.Name = "BttSQRELE"
+        BttSQRELE.Size = New Size(61, 42)
+        BttSQRELE.TabIndex = 19
+        BttSQRELE.Text = "√^x"
+        BttSQRELE.UseVisualStyleBackColor = True
+        ' 
+        ' BttCOS1
+        ' 
+        BttCOS1.Location = New Point(604, 288)
+        BttCOS1.Name = "BttCOS1"
+        BttCOS1.Size = New Size(67, 42)
+        BttCOS1.TabIndex = 36
+        BttCOS1.Text = "cos^-1"
+        BttCOS1.UseVisualStyleBackColor = True
+        ' 
+        ' BttTAN
+        ' 
+        BttTAN.Location = New Point(537, 340)
+        BttTAN.Name = "BttTAN"
+        BttTAN.Size = New Size(60, 42)
+        BttTAN.TabIndex = 35
+        BttTAN.Text = "tan"
+        BttTAN.UseVisualStyleBackColor = True
+        ' 
+        ' BttTAN1
+        ' 
+        BttTAN1.Location = New Point(604, 340)
+        BttTAN1.Name = "BttTAN1"
+        BttTAN1.Size = New Size(67, 42)
+        BttTAN1.TabIndex = 34
+        BttTAN1.Text = "tan^-1"
+        BttTAN1.UseVisualStyleBackColor = True
+        ' 
+        ' BttSIN1
+        ' 
+        BttSIN1.Location = New Point(537, 288)
+        BttSIN1.Name = "BttSIN1"
+        BttSIN1.Size = New Size(60, 42)
+        BttSIN1.TabIndex = 33
+        BttSIN1.Text = "sin^-1"
+        BttSIN1.UseVisualStyleBackColor = True
+        ' 
+        ' BttRAD
+        ' 
+        BttRAD.Location = New Point(537, 237)
+        BttRAD.Name = "BttRAD"
+        BttRAD.Size = New Size(60, 42)
+        BttRAD.TabIndex = 32
+        BttRAD.Text = "Rad "
+        BttRAD.UseVisualStyleBackColor = True
+        ' 
+        ' BttGRAUS
+        ' 
+        BttGRAUS.Location = New Point(604, 237)
+        BttGRAUS.Name = "BttGRAUS"
+        BttGRAUS.Size = New Size(67, 42)
+        BttGRAUS.TabIndex = 31
+        BttGRAUS.Text = "Graus"
+        BttGRAUS.UseVisualStyleBackColor = True
+        ' 
+        ' BttCONTAGENS
+        ' 
+        BttCONTAGENS.Location = New Point(604, 132)
+        BttCONTAGENS.Name = "BttCONTAGENS"
+        BttCONTAGENS.Size = New Size(67, 42)
+        BttCONTAGENS.TabIndex = 30
+        BttCONTAGENS.Text = "nCp"
+        BttCONTAGENS.UseVisualStyleBackColor = True
+        ' 
+        ' BttLOG
+        ' 
+        BttLOG.Location = New Point(537, 184)
+        BttLOG.Name = "BttLOG"
+        BttLOG.Size = New Size(60, 42)
+        BttLOG.TabIndex = 29
+        BttLOG.Text = "log"
+        BttLOG.UseVisualStyleBackColor = True
+        ' 
+        ' BttSD
+        ' 
+        BttSD.Location = New Point(604, 184)
+        BttSD.Name = "BttSD"
+        BttSD.Size = New Size(67, 42)
+        BttSD.TabIndex = 28
+        BttSD.Text = "S-D"
+        BttSD.UseVisualStyleBackColor = True
+        ' 
+        ' BttPERMUTACOES
+        ' 
+        BttPERMUTACOES.Location = New Point(537, 132)
+        BttPERMUTACOES.Name = "BttPERMUTACOES"
+        BttPERMUTACOES.Size = New Size(60, 42)
+        BttPERMUTACOES.TabIndex = 27
+        BttPERMUTACOES.Text = "nPc"
+        BttPERMUTACOES.UseVisualStyleBackColor = True
+        ' 
+        ' BttPERCENTAGEM
+        ' 
+        BttPERCENTAGEM.Location = New Point(537, 81)
+        BttPERCENTAGEM.Name = "BttPERCENTAGEM"
+        BttPERCENTAGEM.Size = New Size(60, 42)
+        BttPERCENTAGEM.TabIndex = 26
+        BttPERCENTAGEM.Text = "%"
+        BttPERCENTAGEM.UseVisualStyleBackColor = True
+        ' 
+        ' BttFATORIAL
+        ' 
+        BttFATORIAL.Location = New Point(604, 81)
+        BttFATORIAL.Name = "BttFATORIAL"
+        BttFATORIAL.Size = New Size(67, 42)
+        BttFATORIAL.TabIndex = 25
+        BttFATORIAL.Text = "n!"
+        BttFATORIAL.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(64), CByte(64), CByte(64))
+        ClientSize = New Size(800, 450)
+        Controls.Add(BttCOS1)
+        Controls.Add(BttTAN)
+        Controls.Add(BttTAN1)
+        Controls.Add(BttSIN1)
+        Controls.Add(BttRAD)
+        Controls.Add(BttGRAUS)
+        Controls.Add(BttCONTAGENS)
+        Controls.Add(BttLOG)
+        Controls.Add(BttSD)
+        Controls.Add(BttPERMUTACOES)
+        Controls.Add(BttPERCENTAGEM)
+        Controls.Add(BttFATORIAL)
+        Controls.Add(BttELEVADO)
+        Controls.Add(BttSIN)
+        Controls.Add(BttCOS)
+        Controls.Add(BttPI)
+        Controls.Add(BttFR)
+        Controls.Add(BttSQRELE)
+        Controls.Add(BttPONTO)
+        Controls.Add(BttC)
+        Controls.Add(BttDIVIDIR)
+        Controls.Add(BttMENOS)
+        Controls.Add(BttSQR)
+        Controls.Add(BttMAIS)
+        Controls.Add(BttIGUAL)
+        Controls.Add(BttVEZES)
+        Controls.Add(Btt7)
+        Controls.Add(Btt9)
+        Controls.Add(Btt0)
+        Controls.Add(Btt6)
+        Controls.Add(Btt8)
+        Controls.Add(Btt3)
+        Controls.Add(Btt4)
+        Controls.Add(Btt5)
+        Controls.Add(Btt2)
+        Controls.Add(Btt1)
+        Controls.Add(TxtBox)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TxtBox As TextBox
+    Friend WithEvents Btt1 As Button
+    Friend WithEvents Btt2 As Button
+    Friend WithEvents Btt5 As Button
+    Friend WithEvents Btt4 As Button
+    Friend WithEvents Btt3 As Button
+    Friend WithEvents Btt8 As Button
+    Friend WithEvents Btt6 As Button
+    Friend WithEvents Btt0 As Button
+    Friend WithEvents Btt9 As Button
+    Friend WithEvents Btt7 As Button
+    Friend WithEvents BttVEZES As Button
+    Friend WithEvents BttIGUAL As Button
+    Friend WithEvents BttMAIS As Button
+    Friend WithEvents BttSQR As Button
+    Friend WithEvents BttMENOS As Button
+    Friend WithEvents BttDIVIDIR As Button
+    Friend WithEvents BttC As Button
+    Friend WithEvents BttPONTO As Button
+    Friend WithEvents BttELEVADO As Button
+    Friend WithEvents BttSIN As Button
+    Friend WithEvents BttCOS As Button
+    Friend WithEvents BttPI As Button
+    Friend WithEvents BttFR As Button
+    Friend WithEvents BttSQRELE As Button
+    Friend WithEvents BttCOS1 As Button
+    Friend WithEvents BttTAN As Button
+    Friend WithEvents BttTAN1 As Button
+    Friend WithEvents BttSIN1 As Button
+    Friend WithEvents BttRAD As Button
+    Friend WithEvents BttGRAUS As Button
+    Friend WithEvents BttCONTAGENS As Button
+    Friend WithEvents BttLOG As Button
+    Friend WithEvents BttSD As Button
+    Friend WithEvents BttPERMUTACOES As Button
+    Friend WithEvents BttPERCENTAGEM As Button
+    Friend WithEvents BttFATORIAL As Button
+
+End Class
---
+++ CALCULADORA  COMPLEXA/Form1.vb
@@ -0,0 +1,377 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+Imports System.Math
+Imports System.Threading
+' A MODIFICAR: Quando for feita a FR, vai aparecer outro visor a mostrai x2=  FEITO
+'              Botão de cancelar nas combinações que crasha    
+'              Aparecer escrito no txtbox o que estou a escrever na trignometria, raiz , raiz de indice X, log e corrigir calc comb
+'              Botão para apagar o ultimo caracter escrito
+'              :D
+Public Class Form1
+    Dim eae, op As Byte
+    Dim n1, n2 As Long
+    Dim isRadians As Boolean = False
+
+
+
+    Private Sub tecla(i As Byte)
+        If eae = 0 Then
+            TxtBox.Text = i
+            eae = 1
+        Else
+            TxtBox.Text = TxtBox.Text & i
+        End If
+    End Sub
+
+    Private Sub TxtBox_TextChanged(sender As Object, e As EventArgs) Handles TxtBox.TextChanged
+        eae = 0
+    End Sub
+
+
+    Private Sub Btt1_Click(sender As Object, e As EventArgs) Handles Btt1.Click
+        tecla(1)
+    End Sub
+
+    Private Sub Btt2_Click(sender As Object, e As EventArgs) Handles Btt2.Click
+        tecla(2)
+    End Sub
+
+    Private Sub Btt3_Click(sender As Object, e As EventArgs) Handles Btt3.Click
+        tecla(3)
+    End Sub
+
+    Private Sub Btt4_Click(sender As Object, e As EventArgs) Handles Btt4.Click
+        tecla(4)
+    End Sub
+
+    Private Sub Btt5_Click(sender As Object, e As EventArgs) Handles Btt5.Click
+        tecla(5)
+    End Sub
+
+    Private Sub Btt6_Click(sender As Object, e As EventArgs) Handles Btt6.Click
+        tecla(6)
+    End Sub
+
+    Private Sub Btt7_Click(sender As Object, e As EventArgs) Handles Btt7.Click
+        tecla(7)
+    End Sub
+
+    Private Sub Btt8_Click(sender As Object, e As EventArgs) Handles Btt8.Click
+        tecla(8)
+    End Sub
+
+    Private Sub Btt9_Click(sender As Object, e As EventArgs) Handles Btt9.Click
+        tecla(9)
+    End Sub
+
+    Private Sub Btt0_Click(sender As Object, e As EventArgs) Handles Btt0.Click
+        tecla(0)
+    End Sub
+
+    Private Sub BttC_Click(sender As Object, e As EventArgs) Handles BttC.Click
+        eae = 0
+        TxtBox.Text = "0"
+        BttGRAUS.BackColor = SystemColors.Control
+        BttRAD.BackColor = SystemColors.Control
+    End Sub
+
+    Private Sub BttPONTO_Click(sender As Object, e As EventArgs) Handles BttPONTO.Click
+        If Not TxtBox.Text.Contains(".") Then
+            TxtBox.Text = TxtBox.Text & "."
+        End If
+    End Sub
+
+    Private Sub BttIGUAL_Click(sender As Object, e As EventArgs) Handles BttIGUAL.Click
+        n2 = Val(TxtBox.Text)
+        Select Case op
+            Case 1
+                TxtBox.Text = Str(n1 + n2)
+            Case 2
+                TxtBox.Text = Str(n1 - n2)
+            Case 3
+                TxtBox.Text = Str(n1 * n2)
+            Case 4
+                TxtBox.Text = Str(n1 / n2)
+            Case 5
+                TxtBox.Text = Str(n1 ^ n2)
+            Case 6
+                TxtBox.Text = Str(If(isRadians, Sin(n1), Sin(n1 * PI / 180)))
+            Case 7
+                TxtBox.Text = Str(If(isRadians, Cos(n1), Cos(n1 * PI / 180)))
+            Case 8
+                TxtBox.Text = Str(If(isRadians, Tan(n1), Tan(n1 * PI / 180)))
+
+            Case 9
+                TxtBox.Text = Str(If(isRadians, Asin(n1), Asin(n1) * 180 / PI))
+            Case 10
+                TxtBox.Text = Str(If(isRadians, Acos(n1), Acos(n1) * 180 / PI))
+            Case 11
+                TxtBox.Text = Str(If(isRadians, Atan(n1), Atan(n1) * 180 / PI))
+            Case 13
+                TxtBox.Text = Str(Log10(n1))
+        End Select
+        eae = 0
+    End Sub
+    '-------------------------------------------------------------------------------------------------------
+    Private Sub BttMAIS_Click(sender As Object, e As EventArgs) Handles BttMAIS.Click
+        op = 1
+        eae = 0
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text &= " + " & n2
+    End Sub
+
+    Private Sub BttMENOS_Click(sender As Object, e As EventArgs) Handles BttMENOS.Click
+        op = 2
+        eae = 0
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text &= " - " & n2
+    End Sub
+
+    Private Sub BttVEZES_Click(sender As Object, e As EventArgs) Handles BttVEZES.Click
+        op = 3
+        eae = 0
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text &= " X " & n2
+
+    End Sub
+
+    Private Sub BttDIVIDIR_Click(sender As Object, e As EventArgs) Handles BttDIVIDIR.Click
+        op = 4
+        eae = 0
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text &= " : " & n2
+
+    End Sub
+
+    '-------------------------------------------------------------------------------------------------------
+
+
+    Private Sub BttSQR_Click(sender As Object, e As EventArgs) Handles BttSQR.Click
+        n1 = Val(TxtBox.Text)
+        'TxtBox.Text = Str(Sqrt(n1))
+        eae = 0
+        TxtBox.Text &= "√" & n1 & "=" & Str(Sqrt(n1))
+
+    End Sub
+
+    Private Sub BttPI_Click(sender As Object, e As EventArgs) Handles BttPI.Click
+        TxtBox.Text = Str(PI)
+        eae = 0
+    End Sub
+
+    Private Sub BttELEVADO_Click(sender As Object, e As EventArgs) Handles BttELEVADO.Click
+        op = 5
+        eae = 0
+        n1 = Val(TxtBox.Text)
+    End Sub
+
+
+    '-------------------------------------------------------------------------------------------------------
+
+    ' trignometria
+    Private Sub BttSIN_Click(sender As Object, e As EventArgs) Handles BttSIN.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "SIN(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 6
+    End Sub
+
+    Private Sub BttCOS_Click(sender As Object, e As EventArgs) Handles BttCOS.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "COS(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 7
+    End Sub
+
+    Private Sub BttTAN_Click(sender As Object, e As EventArgs) Handles BttTAN.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "TAN(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 8
+    End Sub
+
+
+    Private Sub BttSIN1_Click(sender As Object, e As EventArgs) Handles BttSIN1.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "SIN⁻¹(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 9
+    End Sub
+
+    Private Sub BttCOS1_Click(sender As Object, e As EventArgs) Handles BttCOS1.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "COS⁻¹(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 10
+    End Sub
+
+    Private Sub BttTAN1_Click(sender As Object, e As EventArgs) Handles BttTAN1.Click
+        n1 = Val(TxtBox.Text)
+        TxtBox.Text = "TAN⁻¹(" & n1 & If(isRadians, " rad", "°") & ")"
+        op = 11
+    End Sub
+
+
+    ' Alternar radianos e graus
+    Private Sub BttRAD_Click(sender As Object, e As EventArgs) Handles BttRAD.Click
+        isRadians = True
+        BttGRAUS.BackColor = SystemColors.Control
+        BttRAD.BackColor = Color.White
+    End Sub
+
+    Private Sub BttGRAUS_Click(sender As Object, e As EventArgs) Handles BttGRAUS.Click
+        isRadians = False
+        BttGRAUS.BackColor = Color.White
+        BttRAD.BackColor = SystemColors.Control
+    End Sub
+
+
+    '---------------------------------------------------------------------------------------------------------------
+
+
+
+    Private Sub BttLOG_Click(sender As Object, e As EventArgs) Handles BttLOG.Click
+        n1 = Val(TxtBox.Text)
+        op = 13
+        eae = 0
+        'TxtBox.Text &= "Log" & n1 & "=" & op = 13
+    End Sub
+
+
+
+
+    Private Sub BttPERCENTAGEM_Click(sender As Object, e As EventArgs) Handles BttPERCENTAGEM.Click
+        n1 = Val(TxtBox.Text)
+        'TxtBox.Text = Str(n1 / 100)
+        eae = 0
+        TxtBox.Text &= n1 & " : 100 =" & Str(n1 / 100)
+    End Sub
+
+
+    '-------------------------------------------------------------------------------------------------------
+
+    'calculo combinatorio
+    Private Function Fatorial(x As Double) As Double
+        If x = 0 Then
+            Return 1
+        End If
+        Return x * Fatorial(x - 1)
+    End Function
+
+    Private Sub BttFATORIAL_Click(sender As Object, e As EventArgs) Handles BttFATORIAL.Click
+        n1 = Val(TxtBox.Text)
+        Dim result As Double = 1
+        For i = n1 To 1 Step -1
+            n1 = n1 * i
+        Next
+        'TxtBox.Text = Str(n1)
+        TxtBox.Text &= n1 & "!" & "=" & Str(n1)
+        eae = 0
+
+        If n1 < 0 Then
+            MsgBox("Fatorial tem que ser um número positivo e inteiro", vbCritical, "Fatorial")
+        End If
+
+    End Sub
+
+    Private Sub BttPERMUTACOES_Click(sender As Object, e As EventArgs) Handles BttPERMUTACOES.Click
+        n1 = Val(TxtBox.Text)
+        Dim n As Double = InputBox("Valor de c:")
+
+        If n > n1 Then
+            MsgBox("Erro de Sintaxe, p não pode ser > n ", vbCritical, "Permutação ")
+
+            Exit Sub
+            Threading.Thread.Sleep(500)
+
+            BttC_Click(sender, e)
+
+
+        End If
+        ' TxtBox.Text = Str(Fatorial(n1) / Fatorial(n1 - n)) ' nPr
+
+        TxtBox.Text &= n1 & "P" & n & "=" & Str(Fatorial(n1) / Fatorial(n1 - n)) 'nPc
+        eae = 0
+
+    End Sub
+
+    Private Sub BttCONTAGENS_Click(sender As Object, e As EventArgs) Handles BttCONTAGENS.Click
+        n1 = Val(TxtBox.Text)
+        Dim n As Double = InputBox("Valor de p:")
+
+        If n > n1 Then
+            MsgBox("Erro de Sintaxe, p não pode ser > n ", vbCritical, "Permutação ")
+
+            Exit Sub
+            Threading.Thread.Sleep(500)
+
+            BttC_Click(sender, e)
+
+        End If
+
+        'TxtBox.Text = Str(Fatorial(n1) / (Fatorial(n) * Fatorial(n1 - n))) ' nCr
+
+        TxtBox.Text &= n1 & "C" & n & "=" & Str(Fatorial(n1) / (Fatorial(n) * Fatorial(n1 - n))) 'nCp
+        eae = 0
+
+        If n > n1 Then
+            TxtBox.Text = "Erro de Sintaxe, p não pode ser > n "
+
+            '  Module Module1
+            'Sub Main()
+            '    Console.WriteLine(":D")
+            '   Thread.Sleep(5000) ' 5000 milissegundos (5 segundos)
+            '  Console.WriteLine("XD")
+            'End Sub
+            'End Module
+
+            ' BttC.Click()
+
+
+        End If
+        '-------------------------------------------------------------------------------------------------------
+    End Sub
+
+    Private Sub BttSQRELE_Click(sender As Object, e As EventArgs) Handles BttSQRELE.Click ' numero^(1/n)
+        n1 = Val(TxtBox.Text)
+        Dim n As Double = InputBox("Valor de x:")
+
+        ' TxtBox.Text = Str(n1 ^ (1 / n))
+        TxtBox.Text &= "^" & n & "√" & n1 & "=" & Str(n1 ^ (1 / n))
+        eae = 0
+
+    End Sub
+    Private Sub BttFR_Click(sender As Object, e As EventArgs) Handles BttFR.Click ' Formula rosolvente 
+
+
+
+        Dim input As String = InputBox("Escreve a, b e c separados por vírgula:", "Fórmula Resolvente")
+
+        Dim vals() As String = input.Split(","c)
+
+
+        If vals.Length <> 3 Then
+            MsgBox("Escreve os 3 valores separados por vírgulas.", vbCritical, "Fórmula resolvente")
+            Exit Sub
+        End If
+
+        Dim a As Double = Val(vals(0).Trim())
+        Dim b As Double = Val(vals(1).Trim())
+        Dim c As Double = Val(vals(2).Trim())
+
+        ' conteudo = conteudo da rais
+        Dim conteudo As Double = (b ^ 2) - (4 * a * c)
+
+        If conteudo < 0 Then
+            MsgBox("A equação não tem raízes reais.", vbCritical, "Fórmula Resolvente")
+        Else
+
+            Dim x1 As Double = (-b + Sqrt(conteudo)) / (2 * a)
+            Dim x2 As Double = (-b - Sqrt(conteudo)) / (2 * a)
+
+
+            TxtBox.Text = "x1 = " & x1 & vbCrLf & "x2 = " & x2
+
+
+            ' A MODIFICAR: quando for feita a FR, vai aparecer outro visor a mostrai x2= 
+        End If
+    End Sub
+    '--------------------------------------------------------------------------------------------------------------
+    Private Sub BttSD_Click(sender As Object, e As EventArgs) Handles BttSD.Click
+
+    End Sub
+End Class
